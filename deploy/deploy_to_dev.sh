a92adc59227d0d581c193c75fba1a7d20c8ba0df
Dilshat Hewzulla <hewzulla@gmail.com>
2017-07-03 10:53:00 +0100

    add

--- deploy/deploy_to_dev.sh
+++ deploy/deploy_to_dev.sh
@@ -1,3 +1,3 @@
 source /Users/dilshathewzulla/workspace/global-input/global-input-secrets/dev.sh
-echo "deploying the version 1.0.0-SNAPSHOT to root@dev.iterativesolution.co.uk using the property file /Users/dilshathewzulla/workspace/global-input/global-input-secrets/dev.sh (for replacement of the environment specific variables) ..."
-deploy/deploy.sh dev.iterativesolution.co.uk root 1.0.0-SNAPSHOT
+echo "deploying the version 1.0.1-SNAPSHOT to root@dev.iterativesolution.co.uk using the property file /Users/dilshathewzulla/workspace/global-input/global-input-secrets/dev.sh (for replacement of the environment specific variables) ..."
+deploy/deploy.sh dev.iterativesolution.co.uk root 1.0.1-SNAPSHOT
