5be7d6473873b1e3b8802b9b4e43d428de8ed012
Dilshat Hewzulla <hewzulla@gmail.com>
2017-07-14 12:03:52 +0100

    releasing

--- app/messenger.js
+++ app/messenger.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 const minimist     = require('minimist');
 
 var globalInputMessenger={
-    sockets:[],
+    registry:new Map(),
     configPath:"config/config.json",
     init:function(io, argv){
        this.io=io;
@@ -14,7 +14,6 @@ var globalInputMessenger={
        else{
            this.ionamespace=io;
        }
-
        this.ionamespace.on("connect", this.onConnect.bind(this));
     },
     processArguments:function(argv){
@@ -53,43 +52,113 @@ var globalInputMessenger={
         console.log(" GOT Request:::::::::!!!!");
         next(err)
     },
-
+    processError:function(error,message){
+      console.error(error+" "+message);
+      if(error&& error.stack){
+        console.error(error.stack);
+      }
+    },
     onConnect:function(socket){
-        this.sockets.push(socket);
-        console.log(" a user connected:" +socket.id+":"+this.sockets.length);
+        console.log(" a socket connected:" +socket.id);
         var that=this;
         socket.on("disconnect", function(){
-            that.onDisconnect(socket);
+            console.log("the socket is diconnected:"+socket.id);
         });
-        socket.on("sendToSession", this.onReceiveSendMessageToSession.bind(this));
+        socket.on("register", function(registerMessage){
+          console.log("register message is received");
+          try{
+              that.registerSocket(socket,JSON.parse(registerMessage));
+          }
+          catch(error){
+              that.processError(error," in registerSocket");
+          }
+          socket.removeAllListeners("register");
+        });
+        const initiateRegisterMessage={
+              action:"register",
+              socketid:socket.id
+        }
+        socket.emit("register", JSON.stringify(initiateRegisterMessage));
     },
-    onDisconnect:function(socket){
-        var pos=this.sockets.indexOf(socket);
-        if(pos>=0){
-            this.sockets.splice(pos, 1);
-        }
-        console.log("user disconnected:"+socket.id+":"+":"+this.sockets.length);
+
+    registerSocket:function(socket,request){
+            var that=this;
+            const registerItem={
+                socket:socket,
+                application:request.application,
+                session:request.session,
+                client:request.client,
+                time:new Date()
+            };
+            /*
+            if(this.registry.get(registerItem.session)!=null){
+                console.error("session already registered:"+registerItem.session);
+                return;
+            }
+            */
+            this.registry.set(registerItem.session,registerItem);
+            socket.on("disconnect", function(){
+                  that.registry.delete(registerItem.session);
+                  console.log("removed the register:"+registerItem.session+":"+that.registry.size);
+            });
+            console.log("registered:"+registerItem.session+":"+this.registry.size);
+            socket.on("requestToJoin", function(joinMessage){
+              console.log("requestToJoin Message is received:"+joinMessage);
+               try{
+                    that.processJoinMessage(registerItem, JSON.parse(joinMessage));
+                  }
+                  catch(error){
+                    that.processError(error," in requestJoin");
+                  }
+                  socket.removeAllListeners("requestToJoin");
+            });
+
     },
-    onReceiveSendMessageToSession:function(content){
-        try{
-            const message=JSON.parse(content);
-            const session=message.session;
-            delete message.session;
-            var messageToSend=JSON.stringify(message);
+    processJoinMessage:function(registerItem, joinMessage){
 
-            if(session){
-                console.log("sending the message:"+messageToSend +" to:"+session)
-                this.ionamespace.emit(session,messageToSend);
+            const targetClient=this.registry.get(joinMessage.session);
+            if(targetClient==null){
+                console.log("target client does not exist for:"+joinMessage.session);
+                return;
             }
-            else{
-                console.error("onReceiveSendeToClientMessage:missing session in:"+content);
+            if(targetClient.application && targetClient.application !== joinMessage.application){
+              console.log("application does not match:"+joinMessage.application+"!="+targetClient.application);
+              return;
             }
-        }
-        catch(error){
-           console.error("onReceiveSendeToClientMessage:"+error+ " for the content:"+content);
-        }
+            this.listenOnJoinMessageResponse(targetClient,registerItem,joinMessage);
+            targetClient.socket.emit(joinMessage.session+"/join",JSON.stringify(joinMessage));
+    },
+    listenOnJoinMessageResponse:function(targetClient, registerItem,joinMessage){
+      var that=this;
+      targetClient.socket.on("joinMessageResponse", function(response){
+        console.log("joinMessageResponse is received:"+response);
 
-    }
+         try{
+              that.processJoinMessageResponse(registerItem,joinMessage,targetClient,JSON.parse(response));
+            }
+            catch(error){
+              that.processError(error," in  processJoinRequestResult");
+
+            }
+            targetClient.socket.removeAllListeners("joinMessageResponse");
+      });
+    },
+    processJoinMessageResponse:function(registerItem,joinMessage,targetClient,responseMessage){
+          if(responseMessage.allow){
+                const inputMessageListener=function(inputMessage){
+                    targetClient.socket.emit(targetClient.session+"/input",inputMessage);
+                };
+                registerItem.socket.on("inputMessage",inputMessageListener);
+                targetClient.socket.on("disconnect", function(){
+                      registerItem.socket.removeListener("inputMessage",inputMessageListener);
+                      console.log("inputMessage messageListener is removed");
+
+                });
+                registerItem.socket.on("disconnect", function(){
+                  targetClient.socket.emit(targetClient.session+"/leave",joinMessage);
+                });
+          }
+    },
 
 };
 module.exports=globalInputMessenger;
