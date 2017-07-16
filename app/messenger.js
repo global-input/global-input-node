a529acd9ed93d8be83adcae40d0c50982064d22c
Dilshat Hewzulla <hewzulla@gmail.com>
2017-07-16 22:55:34 +0100

    refactored

--- app/messenger.js
+++ app/messenger.js
@@ -2,6 +2,15 @@ var fs = require('fs');
 const minimist     = require('minimist');
 
 var globalInputMessenger={
+  createGUID:function() {
+     function s4() {
+       return Math.floor((1 + Math.random()) * 0x10000)
+         .toString(16)
+         .substring(1);
+     }
+     return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+       s4() + '-' + s4() + s4() + s4();
+   },
     registry:new Map(),
     configPath:"config/config.json",
     init:function(io, argv){
@@ -58,54 +67,68 @@ var globalInputMessenger={
         console.error(error.stack);
       }
     },
+    isApiKeyValid:function(apikey){
+        if(this.config.apikeys.indexOf(apikey)>=0){
+          return true;
+        }
+        else{
+          return false;
+        }
+    },
+
     onConnect:function(socket){
         console.log(" a socket connected:" +socket.id);
-        var that=this;
         socket.on("disconnect", function(){
             console.log("the socket is diconnected:"+socket.id);
         });
+        var that=this;
         socket.on("register", function(registerMessage){
-          console.log("register message is received");
+          console.log("register message is received:"+registerMessage);
           try{
-              that.registerSocket(socket,JSON.parse(registerMessage));
+              that.register(socket,JSON.parse(registerMessage));
           }
           catch(error){
               that.processError(error," in registerSocket");
           }
           socket.removeAllListeners("register");
         });
-        const initiateRegisterMessage={
+        const canRegisterMessage={
               action:"register",
               socketid:socket.id
         }
-        socket.emit("register", JSON.stringify(initiateRegisterMessage));
+        socket.emit("canRegister", JSON.stringify(canRegisterMessage));
     },
+    register:function(socket,request){
+            if(!this.isApiKeyValid(request.apikey)){
+              console.log("apikey is not valid:"+request.apikey)
+              return false;
+            }
 
-    registerSocket:function(socket,request){
             var that=this;
             const registerItem={
                 socket:socket,
-                application:request.application,
+                apikey:request.apikey,
+                sessionGroup:request.sessionGroup,
                 session:request.session,
                 client:request.client,
                 time:new Date()
             };
-            /*
-            if(this.registry.get(registerItem.session)!=null){
-                console.error("session already registered:"+registerItem.session);
-                return;
-            }
-            */
             this.registry.set(registerItem.session,registerItem);
             socket.on("disconnect", function(){
                   that.registry.delete(registerItem.session);
                   console.log("removed the register:"+registerItem.session+":"+that.registry.size);
             });
             console.log("registered:"+registerItem.session+":"+this.registry.size);
-            socket.on("requestToJoin", function(joinMessage){
-              console.log("requestToJoin Message is received:"+joinMessage);
+            var randomkey=this.createGUID();
+            socket.on("joinSession", function(data){
+              console.log("joinSession Message is received:"+data);
                try{
-                    that.processJoinMessage(registerItem, JSON.parse(joinMessage));
+                    const joinSessionMessage=JSON.parse(data);
+                    if(joinSessionMessage.randomkey!==randomkey){
+                      console.log("random key does not match:"+joinSessionMessage.randomkey);
+                      return;
+                    }
+                    that.joinSession(registerItem,joinSessionMessage);
                   }
                   catch(error){
                     that.processError(error," in requestJoin");
@@ -113,39 +136,44 @@ var globalInputMessenger={
                   socket.removeAllListeners("requestToJoin");
             });
 
+            var canJoin={
+                  action:"join",
+                  randomkey
+            }
+            socket.emit("canJoin",JSON.stringify(canJoin));
     },
-    processJoinMessage:function(registerItem, joinMessage){
-
+    joinSession:function(registerItem, joinMessage){
             const targetClient=this.registry.get(joinMessage.session);
             if(targetClient==null){
                 console.log("target client does not exist for:"+joinMessage.session);
                 return;
             }
-            if(targetClient.application && targetClient.application !== joinMessage.application){
-              console.log("application does not match:"+joinMessage.application+"!="+targetClient.application);
+            if(targetClient.sessionGroup !== joinMessage.sessionGroup){
+              console.log("sessionGroup does not match:"+joinMessage.sessionGroup+"!="+targetClient.sessionGroup);
               return;
-            }
+            };
             this.listenOnJoinMessageResponse(targetClient,registerItem,joinMessage);
             targetClient.socket.emit(joinMessage.session+"/join",JSON.stringify(joinMessage));
     },
     listenOnJoinMessageResponse:function(targetClient, registerItem,joinMessage){
       var that=this;
-      targetClient.socket.on("joinMessageResponse", function(response){
-        console.log("joinMessageResponse is received:"+response);
+      targetClient.socket.on("joinResponse", function(response){
+        console.log("joinResponse is received:"+response);
 
          try{
-              that.processJoinMessageResponse(registerItem,joinMessage,targetClient,JSON.parse(response));
+              that.processJoinResponseMessage(registerItem,joinMessage,targetClient,JSON.parse(response));
             }
             catch(error){
               that.processError(error," in  processJoinRequestResult");
 
             }
-            targetClient.socket.removeAllListeners("joinMessageResponse");
+            targetClient.socket.removeAllListeners("joinResponse");
       });
     },
-    processJoinMessageResponse:function(registerItem,joinMessage,targetClient,responseMessage){
+    processJoinResponseMessage:function(registerItem,joinMessage,targetClient,responseMessage){
           if(responseMessage.allow){
                 const inputMessageListener=function(inputMessage){
+                  console.log("forwarding the input message:"+targetClient.session+" message:"+inputMessage);                    
                     targetClient.socket.emit(targetClient.session+"/input",inputMessage);
                 };
                 registerItem.socket.on("inputMessage",inputMessageListener);
@@ -155,9 +183,14 @@ var globalInputMessenger={
 
                 });
                 registerItem.socket.on("disconnect", function(){
-                  targetClient.socket.emit(targetClient.session+"/leave",joinMessage);
+                  targetClient.socket.emit(targetClient.session+"/leave",JSON.stringify(joinMessage));
                 });
+                registerItem.socket.emit("joinResponse",JSON.stringify(responseMessage));
           }
+          else{
+              console.log("join message response says it is not allowed");
+          }
+
     },
 
 };
